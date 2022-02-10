465fbdd752cc7de169107f2399740bb862b5235a
Adevou Fera Mac <ceo@coucoumail.com>
2022-02-10 14:01:26 +0000

    Added Routes for Systems Modules

--- partials-configs.ts
+++ partials-configs.ts
@@ -9,10 +9,37 @@ export const partialConfigs = {
       dashboardHomeRoute: 'dashboard/home',
       homeRoute: 'home',
       settings: 'settings',
-      clientsHomeRoute: 'registration-home',
-      clientsAddEditRoute: 'member-edit',
-      clientsViewRoute: 'member-view',
+      // CLIENTS
+      clientsHomeRoute: 'clients-home',
+      clientsListRoute: 'clients-list',
+      clientsAddEditRoute: 'client-edit',
+      clientsViewRoute: 'client-view',
       procurationsManageRoute: 'procurations-manage',
+
+      // ACCOUNTS
+      accountHomeRoute:'account_home',
+      accountListRoute:'account_list',
+
+      // CAISSE
+      cashierHomeRoute:'cashier_home',
+      cashierListRoute: 'cashier_list',
+      cashierAddRoute: 'cashier_add',
+      cashierOperationAddRoute: 'cashier_operation_add',
+
+      // CREDITS
+      creditsHomeRoute: 'credits_home',
+      creditsListRoute: 'credits_list',
+      creditAddRoute: 'credit_add ',
+
+      // BANQUES
+      bankHomeRoute: 'bank_home',
+      bankListRoute: 'bank_list',
+      bankAddRoute: 'bank_add',
+
+      // DAT
+      datHomeRoute: 'dat_home',
+      datListRoute: 'dat_list',
+      datAddRoute: 'dat_add',
     },
   }
 };
