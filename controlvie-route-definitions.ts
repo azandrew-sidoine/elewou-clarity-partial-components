690fc36bfb5e99df1cd3a56f5ec7bbbbb5746599
benjamin <benbosspayaro@gmail.com>
2022-06-24 21:17:38 +0000

    delete app routes

--- controlvie-route-definitions.ts
+++ controlvie-route-definitions.ts
@@ -1,31 +1,4 @@
 export const routeDefinitions = {
   // --- APP USER ROUTE -----
-  // Données entrantes
-  incomingListRoute: 'incoming/list',
-  incomingEditRoute: 'incoming/edit',
-
-  // Gestion des données de contrôle
-  controlListRoute: 'control/list', // liste des contrôles
-  controlStatsRoute: 'control/stats', // Statistiques des contrôle
-
-  // Gestion des droits et Beneficiaires
-  excelRightsListRoute: 'excel_rights/list',
-  recipientsRoute: 'rights/recipients',
-  recipientsActiveRoute: 'rights/reciepients',
-
-  // Réglages de contrôles
-  settingsRoute: 'settings',
-  migrateDataRoute: 'settings/migrate-data', //Import & Export
-  fingersRoute: 'settings/fingers', //Import & Export
-  addressRoute: 'settings/addresse', //Import & Export
-  rightsTypeRoute: 'settings/rights', // Type of rights
-  devicesRoute: 'settings/devices', //Liste des peripheriques de contrôle
-  editionsRoute: 'settings/editions', // Editions de contrôle de vie
-  sessionsRoute: 'settings/sessions', // Liste des sessions de contrôle par edition
-  dataTypesRoute: 'settings/data-type', // Type de donnees biometriques
-
-  rightsBinding: 'right-binding', // liaison des droits
-
-  enrollement: 'enrollement', // enrollement
   geolocation: 'geolocation',
 };
