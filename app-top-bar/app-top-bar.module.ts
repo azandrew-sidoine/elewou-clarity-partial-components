0f4d79dc51c7f699876ddd3cbc8b9a126781bb7e
azandrew-sidoine <azandrewdevelopper@gmail.com>
2023-07-24 18:21:25 +0000

    Added Router module fixes for sidebar and topbar

--- app-top-bar/app-top-bar.module.ts
+++ app-top-bar/app-top-bar.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ClarityModule } from "@clr/angular";
 import { AppTopBarComponent } from "./app-top-bar.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  imports: [CommonModule, ClarityModule],
+  imports: [CommonModule, ClarityModule, RouterModule],
   declarations: [AppTopBarComponent],
   exports: [AppTopBarComponent],
 })
--- sidebar/sidebar.module.ts
+++ sidebar/sidebar.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SidebarComponent } from "./sidebar.component";
 import { ClarityModule } from "@clr/angular";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  imports: [CommonModule, ClarityModule],
+  imports: [CommonModule, ClarityModule, RouterModule],
   exports: [SidebarComponent],
   declarations: [SidebarComponent],
   providers: [],
